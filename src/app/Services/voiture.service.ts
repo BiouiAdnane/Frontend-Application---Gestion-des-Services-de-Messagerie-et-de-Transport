f9a255c80cfbe5f80a4deb0696fe27d6750ed6c3
Adnane <biouiadnane@gmail.com>
2023-08-01 22:57:52 +0100

    vehicule service done

--- src/app/Services/voiture.service.ts
+++ src/app/Services/voiture.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Conducteur} from "../EnititeComponent/Models/Conducteur";
+import {Voiture} from "../EnititeComponent/Models/Voiture";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoitureService {
 
-  constructor() { }
+
+  backendHost :String="http://localhost:8080"
+  constructor(private http:HttpClient) { }
+
+  public listVehicules():Observable<Array<Voiture>>{
+    return this.http.get<Array<Voiture>>(this.backendHost+"/voitures")
+  }
+
+  public getVoiture(code_Voiture : number):Observable<Voiture>{
+    return this.http.get<Voiture>(this.backendHost+"/voitures/"+ code_Voiture)
+  }
+
+
+  public saveVehicule(voiture : Voiture){
+    return this.http.post(this.backendHost + "/voitures" ,voiture)
+  }
+  public deleteVehicule(code_Voiture : number){
+    return this.http.delete(this.backendHost+"/voitures/"+code_Voiture)
+  }
+
+  
 }
