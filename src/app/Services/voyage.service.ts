95cad0048cd22d83bce32c67918757845af59c0e
Adnane <biouiadnane@gmail.com>
2023-08-06 15:46:10 +0100

    VoyageService done

---
+++ src/app/Services/voyage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Voiture} from "../EnititeComponent/Models/Voiture";
+import {Voyage} from "../EnititeComponent/Models/Voyage";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoyageService {
+
+  backendHost :String="http://localhost:8080"
+  constructor(private http:HttpClient) { }
+
+  public listVoyages():Observable<Array<Voyage>>{
+    return this.http.get<Array<Voyage>>(this.backendHost+"/voyages")
+  }
+
+  public getVoyage(codeVoyage : number):Observable<Voyage>{
+    return this.http.get<Voyage>(this.backendHost+"/voyages/"+ codeVoyage)
+  }
+
+  public saveVoyage(voyage : Voyage){
+    return this.http.post(this.backendHost + "/voyages" ,voyage)
+  }
+  public deleteVoyage(codeVoyage : number){
+    return this.http.delete(this.backendHost+"/voyages/"+codeVoyage)
+  }
+}
