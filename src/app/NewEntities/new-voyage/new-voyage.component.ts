249599a0da561e580b947fe469f1efc26906e7d4
Adnane <biouiadnane@gmail.com>
2023-08-08 17:04:48 +0100

    new trip qmise en forme V1

--- src/app/NewEntities/new-voyage/new-voyage.component.ts
+++ src/app/NewEntities/new-voyage/new-voyage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {Conducteur} from "../../EnititeComponent/Models/Conducteur";
 
 @Component({
   selector: 'app-new-voyage',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-voyage.component.css']
 })
 export class NewVoyageComponent {
+  newVoayageFormGroup!: FormGroup;
+  conducteurs!:Conducteur;
 
+  handleSaveVoyage() {
+
+  }
 }
